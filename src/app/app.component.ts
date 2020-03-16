9ff994966e03a3bae5a92207704970ffdaa7db0a
Sri Harsha Chinni <sriharshachinni@gpadmins-MacBook-Pro.local>
2020-03-15 21:13:17 -0600

    Adds the second table tag to the app component with a different data set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,44 @@ export class AppComponent {
       skillset: 'Infect people'
     },
   ];
+
+  anotherTableCols = ['city', 'state', 'country', 'population'];
+  anotherTableData = [
+    {
+      city: 'Hyderabad',
+      state: 'Telangana',
+      country: 'India',
+      population: '10M'
+    },
+    {
+      city: 'Salt Lake City',
+      state: 'Utah',
+      country: 'USA',
+      population: '300k'
+    },
+    {
+      city: 'Chennai',
+      state: 'Tamil Nadu',
+      country: 'India',
+      population: '10M'
+    },
+    {
+      city: 'New York City',
+      state: 'New York',
+      country: 'USA',
+      population: '5M'
+    },
+    {
+      city: 'Nellore',
+      state: 'Andhra Pradesh',
+      country: 'India',
+      population: '3M'
+    },
+    {
+      city: 'San Fransico',
+      state: 'California',
+      country: 'USA',
+      population: '1M'
+    },
+  ];
 }
